a33617c54a3ee6b2d199399c8f59ea0c9af6ffbb
Lucien Zhang <lucien_zhang@hotmail.com>
2019-12-15 21:57:14 +0900

    ml

--- ml/static/js/ml.js
+++ ml/static/js/ml.js
@@ -1,6 +1,5 @@
-/*
-* @Author: Lucien Zhang
-* @Date:   2019-09-20 13:22:59
-* @Last Modified by:   Lucien Zhang
-* @Last Modified time: 2019-09-20 13:22:59
-*/
+$(document).ready(function () {
+    $('#sidebarCollapse').on('click', function () {
+        $('#sidebar').toggleClass('active');
+    });
+});
